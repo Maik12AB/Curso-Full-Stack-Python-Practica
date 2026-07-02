8ec36ed28c14ae5a35ce9e9328a3c5e69d11fa1c
Maikol PC <maikol.a.b2015@gmail.com>
2026-07-02 13:01:17 -0400

    refactorizarp18

--- src/p18-funciones1.py
+++ src/p18-funciones1.py
@@ -1,3 +1,4 @@
+import sys
 # Codigo original: src/sumar-dos-numeros.py
 # Hacemos que trabaje con funciones
 # Refactorizacion
@@ -15,17 +16,22 @@ def msj_error(texto,valor_error):
     print( "Los valores ingresados deben ser números enteros" )
     print( f"{texto} numero es:  [{valor_error}]" )
 
+def sumar(n1,n2):
+    return n1 + n2
+
 numero1 = input("Ingrese el primer número entero:")
 numero2 = input("Ingrese el segundo número entero:")
 
 if not es_entero(numero1):
     msj_error("Primero", numero1)
-
+    sys.exit()
 if not es_entero(numero2):
     msj_error("Segundo", numero2)
+    sys.exit()
 
-if numero1.isdigit() and numero2.isdigit():
-    numero_1 = int( numero1 )
-    numero_2 = int( numero2 )
+numero_1 = int( numero1 )
+numero_2 = int( numero2 )
 
-    suma = numero_1 + numero_2
+suma = sumar(numero_1, numero_2)
+
+print(f"La suma de los numeros es: {suma}")
