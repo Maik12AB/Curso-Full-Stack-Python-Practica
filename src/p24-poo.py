fcde2a50f6e5d468e8658ed0eb6c627b712e80d1
Maikol PC <maikol.a.b2015@gmail.com>
2026-07-08 22:15:02 -0400

    POO

--- src/p24-poo.py
+++ src/p24-poo.py
@@ -2,10 +2,14 @@ from p24_poo_clase_medicamento import Medicamento
 
 def main():
     medicamento_nuevo = Medicamento()
-    print(f"Precio inicial: {medicamento_nuevo.precio}")
-    precio = int(input("Ingrese el precio del medicamento: "))
+    print( f"Precio inicial {medicamento_nuevo.precio}" )
+
+    precio = int( input("Ingrese precio del medicamento: ") )
+
     medicamento_nuevo.asignar_precio(precio)
-    print(f"Precio nuevo {medicamento_nuevo.precio}")
+
+    print( f"Precio nuevo {medicamento_nuevo.precio} y descuento {medicamento_nuevo.descuento}" )
+
 
 if __name__ == '__main__':
     main()
