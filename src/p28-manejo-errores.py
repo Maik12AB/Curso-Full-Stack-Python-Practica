9e6ec8fa0c5ab80f516bf8997444afca9542896e
Maikol PC <maikol.a.b2015@gmail.com>
2026-07-23 20:25:17 -0400

    tipos de error

---
+++ src/p28-manejo-errores.py
@@ -0,0 +1,29 @@
+# Errores de sintaxis
+#--------------------
+# Estos errores impiden que el programa se ejecute. Son detectados por el intérprete de Python y se muestran en la consola.
+# edad = 20
+
+# if edad > 18:
+# print("Eres mayor de edad")
+
+# Errores en tiempo de ejecución
+#------------------------------
+# Ocurren durante la ejecucion del programa
+#cuando queremos realizar una operación que no es válida, como dividir por cero o acceder a un índice fuera de rango en una lista.
+
+# n1 = 10
+# n2 = 0
+# r=n1 / n2  # Esto generará un error de división por cero
+
+
+# Errores de lógica
+#--------------------
+#son errores que no generan un mensaje de error,
+#  pero hacen que el programa no funcione como se esperaba.
+
+# precio=10_000
+# descuento=20
+# precio_venta=precio-descuento
+# print(f"El precio de venta es: {precio_venta}")  # Esto imprimirá 9980, pero el resultado esperado era 8000
+
+
